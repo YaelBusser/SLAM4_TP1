5f0348f48b026355d4d367007054e18aceaabea7
Yaël Busser <mrmaclevine@gmail.com>
2022-09-08 16:47:13 +0200

    ajout

---
+++ controllers/SampleWeb.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace controllers;
+
+use controllers\base\Web;
+use utils\Template;
+
+class SampleWeb extends Web
+{
+    function home()
+    {
+        Template::render("views/global/home.php", array("date" => date("d-m-Y à H:i")));
+    }
+    function about(){
+        Template::render("views/global/about.php", array("titre" => "À Propos"));
+    }
+}
